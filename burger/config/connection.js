843f606b9809bb14ad09d78fc6c14700f47b29ad
essiemik07 <34856841+essiemik07@users.noreply.github.com>
2018-06-09 19:02:34 -0700

    Update connection.js

--- burger/config/connection.js
+++ burger/config/connection.js
@@ -1,12 +1,16 @@
 // Set up MySQL connection.
 var mysql = require("mysql");
+var connection;
 
-var connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "",
-  database: "burgers_db"
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
+   host: "qzkp8ry756433yd4.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
+   port: 3306,
+   user: "xlpecvw2csiwk6ds",
+   password: "dgqrkvt29idq42eq",
+   database: "pzkxvh6x56c4g5s4"
 });
 
 // Make connection.
